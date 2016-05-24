1c524f94fd46a7a48d33f29c4fb35385d59b0758
Michał Wyrzykowski <michal.wyrzykowski@spartez.com>
2016-05-24 06:28:57 +0200

    Fix directory in java8 docker. Add clean script.

---
+++ scripts/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CONTAINER_NAME=$1
+IMAGE=$2
+DOCKERS=`docker ps -a | grep $CONTAINER_NAME | cut -d " " -f 1`
+docker stop $DOCKERS \
+&& docker rm -f $DOCKERS \
+&& docker rmi -f $IMAGE
