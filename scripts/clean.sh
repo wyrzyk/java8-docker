8e7bee28fa3aa03e709956c0b30046be6c72a61f
Michał Wyrzykowski <michal.wyrzykowski@spartez.com>
2016-05-25 05:40:25 +0200

    Fix clean script.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,6 +3,7 @@
 CONTAINER_NAME=$1
 IMAGE=$2
 DOCKERS=`docker ps -a | grep $CONTAINER_NAME | cut -d " " -f 1`
+
 docker stop $DOCKERS \
-&& docker rm -f $DOCKERS \
-&& docker rmi -f $IMAGE
+; docker rm -f $DOCKERS \
+; docker rmi -f $IMAGE
